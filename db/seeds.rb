c3702993c4225aa2cb45d58a0ea5954482659468
rails --v <ruby -v>
2023-08-12 21:50:10 +0300

    Add database configuration.

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+puts 'start'
+Greeting.create(message: 'Hello everyone!')
+Greeting.create(message: 'Hi Friend!')
+Greeting.create(message: 'Greetings all!')
+Greeting.create(message: 'Hope you are good!')
+Greeting.create(message: 'Hey!')
+puts 'end'
